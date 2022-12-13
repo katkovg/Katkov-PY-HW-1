0e7145afaf40fd41ddae6d05c3607f7bb10acd22
katkovg <83498975+katkovg@users.noreply.github.com>
2022-12-13 23:15:23 +0300

    Correct task2.py

--- task2.py
+++ task2.py
@@ -1,9 +1,5 @@
 # Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-def func (X, Y, Z):
-    return print(f'X = {X}, Y = {Y}, Z = {Z}.\nnot (True or True or True) = {not (X or Y or Z)}.\nnot X and not Y and not Z = {not X and not Y and not Z}.\n')
-
-
 X = int(input('Введите любое число X: '))
 Y = int(input('Введите любое число Y: '))
 Z = int(input('Введите любое число Z: '))
